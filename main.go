3026c04ca0f3c5d1e6773abe0dc8d574b5494980
Galen Burghardt <galen.w.burghardt@gmail.com>
2015-10-23 11:12:39 -0500

    undoing stuff

--- main.go
+++ main.go
@@ -66,7 +66,10 @@ func readAnonymousMessage(r *http.Request) string {
 		return "Slack bug; inform the team."
 	}
 	msg := strings.TrimSpace(r.Form[keyText][0])
-	
+	matches := payloadExp.FindStringSubmatch(msg)
+	if matches == nil {
+		return "Failed; message should be like: /anon @ashwin hey what's up?"
+	}
 	user := matches[1]
 	msg = strings.TrimSpace(matches[2])
 	err = sendAnonymousMessage(user, msg)
